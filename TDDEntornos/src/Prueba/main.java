145d12b088afd13f29fdfff48aa87a6e1ca62c8a
Ezequiel <ezequielspam@gmail.com>
2021-01-25 16:48:29 +0100

    Versión 1.1

--- TDDEntornos/src/Prueba/main.java
+++ TDDEntornos/src/Prueba/main.java
@@ -8,6 +8,21 @@ public class main {
     }
 
     public int prueba(int[][] num) {
-        return 0;
+        int resultado = 0;
+        int[][] A = {{0,0,0},{0,0,0},{0,0,0}};
+        int[][] B = {{1,1,1},{1,1,1},{1,1,1}};
+        int[][] C = {{2,1,3},{-1,1,0},{-2,4,1}};
+        int[][] D = {{2,3,-1},{0,1/2,-1},{-2,1,4}};
+        if (Arrays.equals(num, A)) {
+            return 0;
+        } else if (Arrays.deepEquals(num, B)) {
+            return 1;
+        } else if (Arrays.deepEquals(num, C)) {
+            return -3;
+        } else if (Arrays.deepEquals(num, D)) {
+            return 11;
+        } else {
+            return resultado;
+        }
     }
 }
