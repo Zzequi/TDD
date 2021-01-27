797fae11f7dfd4a2b3cb2236fa02a7c7d8421fc0
ezequiel <ezequielspam@gmail.com>
2021-01-27 13:35:40 +0100

    Versión 2.1

--- TDDEntornos/src/Prueba/main.java
+++ TDDEntornos/src/Prueba/main.java
@@ -1,19 +1,12 @@
 package Prueba;
 
-import java.util.Arrays;
-
 public class main {
-    public static void main(String[] args){
-
-    }
-
-    public float prueba(float[][] num) {
+    public float test(float[][] num) {
         float[] uno = num[0];
         float[] dos = num[1];
         float[] tres = num[2];
-        float restar1 = (uno[0] * dos[1] * tres[2]) + (uno[1] * dos[2] * tres[0]) + (uno[2] * dos[0] * tres[1]);
-        float restar2 = (tres[0] * dos[1] * uno[2]) + (tres[1] * dos[2] * uno[0]) + (tres[2] * dos[0] * uno[1]);
-        float resultado = restar1 - restar2;
-        return resultado;
+        float minus1 = (uno[0] * dos[1] * tres[2]) + (uno[1] * dos[2] * tres[0]) + (uno[2] * dos[0] * tres[1]);
+        float minus2 = (tres[0] * dos[1] * uno[2]) + (tres[1] * dos[2] * uno[0]) + (tres[2] * dos[0] * uno[1]);
+        return minus1 - minus2;
     }
 }
--- TDDEntornos/src/Prueba/mainTest.java
+++ TDDEntornos/src/Prueba/mainTest.java
@@ -8,32 +8,32 @@ class mainTest extends main{
     @Test
     void testA() {
         float[][] A = {{0,0,0},{0,0,0},{0,0,0}};
-        float actual = prueba(A);
-        float esperado = 0;
-        assertEquals(esperado, actual);
+        float actual = test(A);
+        float expected = 0;
+        assertEquals(expected, actual);
     }
 
     @Test
     void testB() {
         float[][] B = {{1,1,1},{1,1,1},{1,1,1}};
-        float actual = prueba(B);
-        float esperado = 0;
-        assertEquals(esperado, actual);
+        float actual = test(B);
+        float expected = 0;
+        assertEquals(expected, actual);
     }
 
     @Test
     void testC() {
         float[][] C = {{2,1,3},{-1,1,0},{-2,4,1}};
-        float actual = prueba(C);
-        float esperado = -3;
-        assertEquals(esperado, actual);
+        float actual = test(C);
+        float expected = -3;
+        assertEquals(expected, actual);
     }
 
     @Test
     void testD() {
         float[][] D = {{2,3,-1},{0,0.5f,-1},{-2,1,4}};
-        float actual = prueba(D);
-        float esperado = 11;
-        assertEquals(esperado, actual);
+        float actual = test(D);
+        float expected = 11;
+        assertEquals(expected, actual);
     }
 }
